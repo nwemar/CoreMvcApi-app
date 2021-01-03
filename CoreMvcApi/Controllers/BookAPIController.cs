f314a22a35fbfd3559c40e3238748b959761629f
nwemar <capital.nwemarwin@gmail.com>
2021-01-03 13:49:07 +0630

    first commit

---
+++ CoreMvcApi/Controllers/BookAPIController.cs
@@ -0,0 +1,38 @@
+﻿using CoreMvcApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvcApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BookAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ActionName("GetBookList")]
+        public IActionResult GetBookList()
+        {
+            List<Book_Class> book_Lst = new List<Book_Class>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Book_Class obj = new Book_Class();
+                obj.id = i + 1;
+                if (i == 0)
+                    obj.Title = "C#";
+                if (i == 1)
+                    obj.Title = "PHP";
+                if (i == 2)
+                    obj.Title = "Ruby";
+                obj.Price = 500;
+                obj.Description = obj.Title + " BOOK";
+                book_Lst.Add(obj);
+            }
+            return Ok(book_Lst);
+        }
+    }
+}
---
+++ CoreMvcApi/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using CoreMvcApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvcApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ActionName("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
